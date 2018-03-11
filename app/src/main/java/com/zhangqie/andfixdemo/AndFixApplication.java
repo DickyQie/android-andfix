7d9b3ad8d5ac499e35200d920930174611f4aa20
DickyQie <18523978916@163.com>
2018-03-11 16:20:16 +0800

    阿里AndFix热修复

---
+++ app/src/main/java/com/zhangqie/andfixdemo/AndFixApplication.java
@@ -0,0 +1,14 @@
+package com.zhangqie.andfixdemo;
+
+import android.app.Application;
+
+/**
+ * Created by zhangqie on 2018/3/11.
+ */
+
+public class AndFixApplication extends Application {
+
+
+
+
+}
---
+++ app/src/main/java/com/zhangqie/andfixdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zhangqie.andfixdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/****
+ * 阿里 AndFix热修复
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
