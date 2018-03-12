3cfcea9a06595216efd3e75df7c5b5464f9cbb05
DickyQie <18523978916@163.com>
2018-03-12 11:16:08 +0800

    bug消息提示，Application修改，apatch补丁文件生成

--- app/src/main/java/com/zhangqie/andfixdemo/AndFixApplication.java
+++ app/src/main/java/com/zhangqie/andfixdemo/AndFixApplication.java
@@ -1,6 +1,12 @@
 package com.zhangqie.andfixdemo;
 
 import android.app.Application;
+import android.os.Environment;
+import android.util.Log;
+
+import com.alipay.euler.andfix.patch.PatchManager;
+
+import java.io.IOException;
 
 /**
  * Created by zhangqie on 2018/3/11.
@@ -8,7 +14,34 @@ import android.app.Application;
 
 public class AndFixApplication extends Application {
 
+    public static PatchManager mPatchManager;
+
+    public String TAG = "Application";
+
+    private static final String APATCH_PATH = "";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 初始化patch管理类
+        mPatchManager = new PatchManager(this);
+        // 初始化patch版本
+        mPatchManager.init("1.0");
+
+        mPatchManager.loadPatch();
 
 
+        try {
+            // .apatch file path ，这里一定要注意每台手机sd卡路径不同
+            String patchFileString =  Environment.getExternalStorageDirectory().getAbsolutePath()  + APATCH_PATH;
+            Log.i(TAG, "onCreate: "+patchFileString);
+            //3）添加patch
+            mPatchManager.addPatch(patchFileString);
+            Log.d(TAG, "apatch:" + patchFileString + " added.");
+        } catch (IOException e) {
+            Log.e(TAG, "", e);
+        }
 
+
+    }
 }
--- app/src/main/java/com/zhangqie/andfixdemo/MainActivity.java
+++ app/src/main/java/com/zhangqie/andfixdemo/MainActivity.java
@@ -2,15 +2,37 @@ package com.zhangqie.andfixdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /****
  * 阿里 AndFix热修复
  */
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
+
+
+    private void initView(){
+        textView = (TextView) findViewById(R.id.textview);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"bug未修复",Toast.LENGTH_LONG).show();
+                textView.setText("bug未修复");
+            }
+        });
+    }
+
+
+
+
 }
